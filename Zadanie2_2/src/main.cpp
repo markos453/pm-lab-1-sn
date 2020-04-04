6abc59b9bb4995fc6a291dcff29d1709b01aa3fa
Maciej <maciej.markowski@student.wat.edu.pl>
2020-04-04 14:48:18 +0200

    „Zakończono zadanie 2.2.3”.

--- Zadanie2_2/src/main.cpp
+++ Zadanie2_2/src/main.cpp
@@ -2,16 +2,37 @@
 
 #define LED_RED
 
+#define M_KKROPA_T 200
+#define M_KRESKA_T 3 * M_KKROPA_T
+#define M_PAUZA_T 3 * M_KKROPA_T
+#define M_SPACJA_T 7 * M_KROPKA_T
+
 void setup() {
-  // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  for (int i =0; i < 3; i++)
+  {
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
+  delay(M_KKROPA_T;
   digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
+  delay(M_PAUZA_T);
+  }
+  for (int i =0; i < 3; i++)
+  {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(M_KRESKA_T);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(M_PAUZA_T);
+  }
+   for (int i =0; i < 3; i++)
+   {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(M_KRESKA_T);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(M_PAUZA_T);
+   }
+   delay(M_SPACJA_T);
 }
