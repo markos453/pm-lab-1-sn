10051f057b8839d7f1d07507100401b2be8de87b
Maciej <maciej.markowski@student.wat.edu.pl>
2020-04-04 14:51:01 +0200

    poprawka do 2.2.3

--- Zadanie2_2/src/main.cpp
+++ Zadanie2_2/src/main.cpp
@@ -2,7 +2,7 @@
 
 #define LED_RED
 
-#define M_KKROPA_T 200
+#define M_KROPA_T 200
 #define M_KRESKA_T 3 * M_KKROPA_T
 #define M_PAUZA_T 3 * M_KKROPA_T
 #define M_SPACJA_T 7 * M_KROPKA_T
@@ -16,7 +16,7 @@ void loop()
   for (int i =0; i < 3; i++)
   {
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(M_KKROPA_T;
+  delay(M_KKROPA_T);
   digitalWrite(LED_BUILTIN, LOW);
   delay(M_PAUZA_T);
   }
