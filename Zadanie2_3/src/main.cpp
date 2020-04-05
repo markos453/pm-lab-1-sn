cb143d3e5f38ea5f54ee39659884e1ae3e39e23c
Maciej <maciej.markowski@student.wat.edu.pl>
2020-04-05 15:19:05 +0200

    wykonano zadanie 2-3-2

--- Zadanie2_3/src/main.cpp
+++ Zadanie2_3/src/main.cpp
@@ -1,97 +1,42 @@
 #include <Arduino.h>
-#define CAR_RED 12
-#define CAR_YELLOW 11
-#define CAR_GREEN 10
-#define PED_RED 9
-#define PED_GREEN 8
 
-#define PED_BUTTON 2
+uint8_t ledPin[] = {4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+uint8_t ledDelay = 65;
+uint8_t direction = 1;
+uint8_t currentLED = 0;
+unsigned long changeTime;
 
-#define CHANGE TIME 10000
-#define STATE_1_TIME 10000
-#define STATE_2_TIME 1000
-#define STATE_3_TIME 7000
-#define STATE_4_TIME 1000
+void changeLED();
 
-enum Lightstate_t {GREEN, YELLOW, YELLOW_RED, RED};
-Lightstate_t lightState = GREEN;
-unsigned long changeTime = 0;
-
-void setup() {
-  pinMode(CAR_RED, OUTPUT);
-  pinMode(CAR_YELLOW, OUTPUT);
-  pinMode(CAR_GREEN, OUTPUT);
-  pinMode(PED_RED, OUTPUT);
-  pinMode(PED_GREEN, OUTPUT);
-  pinMode(PED_BUTTON, INPUT);
-
-  digitalWrite(CAR_RED, LOW);
-  digitalWrite(CAR_YELLOW, LOW);
-  digitalWrite(CAR_GREEN, HIGH);
-  digitalWrite(PED_RED, HIGH);
-  digitalWrite(PED_GREEN, LOW);
-  changeTime = millis();
+void setup()
+{
+  for (int i = 0; i < 10; i++)
+  {
+    pinMode(ledPin[i], OUTPUT);
+  }
 }
-
-void loop() {
- uint8_t state = digitalRead(PED_BUTTON);
- switch (Lightstate_t)
- {
-   case GREEN:
-     digitalWrite(CAR_RED, LOW);
-  digitalWrite(CAR_YELLOW, LOW);
-  digitalWrite(CAR_GREEN, HIGH);
-  digitalWrite(PED_RED, HIGH);
-  digitalWrite(PED_GREEN, LOW);
-  if(((millis() - changeTime) >STATE_1_TIME) || (state == HIGH))
+void loop()
+{
+  if ((millis() - changeTime) > ledDelay)
   {
-    lightState = YELLOW;
+    changeLED();
     changeTime = millis();
   }
-  break;
-case YELLOW:
-   case GREEN:
-     digitalWrite(CAR_RED, LOW);
-  digitalWrite(CAR_YELLOW, HIGH);
-  digitalWrite(CAR_GREEN, LOW);
-  digitalWrite(PED_RED, HIGH);
-  digitalWrite(PED_GREEN, LOW);
-  if(((millis() - changeTime) >STATE_2_TIME) || (state == HIGH))
+}
+void changeLED()
+{
+  for (int x = 0; x <10; x++)
+  digitalWrite(ledPin[x], LOW);
   {
-    lightState = RED;
-    changeTime = millis();
-  }
-  break;
-  case RED:
-     case GREEN:
-     digitalWrite(CAR_RED, LOW);
-  digitalWrite(CAR_YELLOW, LOW);
-  digitalWrite(CAR_GREEN, HIGH);
-  digitalWrite(PED_RED, LOW);
-  digitalWrite(PED_GREEN, HIGH);
-  if(((millis() - changeTime) >STATE_3_TIME) || (state == HIGH))
-  {
-    for(uint8_t i = 0; i < 3; i++)
+    digitalWrite(ledPin[currnetLED], HIGH);
+    currentLED += direction;
+    if (currentLED == 9)
     {
-      digitalWrite(PED_GREEN, LOW);
-      delay(500);
-      digitalWrite(PED_GREEN, HIGH);
-      delay(500);
+      direction = -1;
     }
-    lightState = YELLOW_RED;
-    changeTime = millis();
+    if (currentLED == 0)
+    {
+      direction = 1;
+    }
   }
-  break;
-  case YELLOW_RED:
-  digitalWrite(CAR_RED, LOW);
-  digitalWrite(CAR_YELLOW, HIGH);
-  digitalWrite(CAR_GREEN, HIGH);
-  digitalWrite(PED_RED, HIGH);
-  digitalWrite(PED_GREEN, LOW);
-  if(((millis() - changeTime) >STATE_4_TIME)
-  {
-    lightState = GREEN;
-    changeTime = millis();
-  }
-break;
- }
+}
